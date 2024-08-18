af48024ae6d9744eca2c4c9d63ec7e2469898cdf
Emanuel Jose Zomoza <ema_z18@hotmail.com>
2024-08-17 23:10:33 -0300

    logica para reservas + modal con accion para reservar viaje. #12

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -112,3 +112,126 @@ dots.forEach((li, key) => {
   });
 });
 // SLIDER CLOSED
+
+// RESERVAS
+const productContainers = [...document.querySelectorAll(".product-container")]
+const nxtBtn = [...document.querySelectorAll(".nxt-btn")]
+const preBtn = [...document.querySelectorAll(".pre-btn")]
+
+productContainers.forEach((item, i) => {
+  // Width tarjeta + margen
+  const cardWidth = item.querySelector(".product-card").offsetWidth + 20
+
+  nxtBtn[i].addEventListener("click", () => {
+    item.scrollLeft += cardWidth
+  })
+
+  preBtn[i].addEventListener("click", () => {
+    item.scrollLeft -= cardWidth
+  })
+})
+
+// MODAL
+// Para "Ver más"
+const cardButtons = document.querySelectorAll(".card-btn")
+const modal = document.getElementById("myModal")
+const modalImg = document.getElementById("modal-img")
+const modalDescription = document.getElementById("modal-description")
+const closeModal = document.querySelector(".close")
+const adultsInput = document.getElementById("adults")
+const minusAdults = document.getElementById("minus-adults")
+const plusAdults = document.getElementById("plus-adults")
+
+// abre el modal
+document.querySelectorAll(".card-btn").forEach((btn, i) => {
+  btn.addEventListener("click", function() {
+    const productCard = btn.closest(".product-card");
+
+    const description = productCard.querySelector(".product-info .product-short-description").innerText;
+    const videoSrc = productCard.querySelector(".product-vid video").getAttribute("src");
+
+    const modal = document.getElementById("myModal");
+    const modalVid = modal.querySelector("#modal-vid video");
+    const modalDescription = modal.querySelector("#modal-description");
+
+    modalVid.setAttribute("src", videoSrc);
+    modalDescription.innerText = description;
+
+    modal.style.display = "block";
+    setTimeout(() => {
+      modal.style.opacity = "1";
+      document.querySelector(".modal-content").style.opacity = "1";
+    }, 10);
+  });
+});
+
+// cerrar modal
+document.querySelector(".close").addEventListener("click", function() {
+  const modal = document.getElementById("myModal");
+  
+  modal.style.opacity = "0";
+  modal.querySelector(".modal-content").style.opacity = "0";
+  setTimeout(() => {
+    modal.style.display = "none";
+    modal.querySelector("#modal-vid video").setAttribute("src", "");
+  }, 500);
+});
+
+// cerrar modal fuera
+window.addEventListener("click", function(event) {
+  const modal = document.getElementById("myModal");
+  if (event.target == modal) {
+    modal.style.opacity = "0";
+    modal.querySelector(".modal-content").style.opacity = "0";
+    setTimeout(() => {
+      modal.style.display = "none";
+      modal.querySelector("#modal-vid video").setAttribute("src", "");
+    }, 500);
+  }
+});
+
+
+// adultos limit
+minusAdults.addEventListener("click", () => {
+  if (adultsInput.value > 1) {
+    adultsInput.value--;
+  }
+});
+
+plusAdults.addEventListener("click", () => {
+  if (adultsInput.value < 5) {
+    adultsInput.value++;
+  }
+});
+
+// Reservar ahora
+const handleReservation = (event) => {
+  event.preventDefault();
+  
+  const reserveBtn = event.target;
+
+  reserveBtn.textContent = "Viaje reservado";
+  reserveBtn.classList.add("reserved");
+  
+  setTimeout(() => {
+    closeModalFunc();
+    setTimeout(() => {
+      reserveBtn.textContent = "Reservar ahora";
+      reserveBtn.classList.remove("reserved");
+    }, 500);
+  }, 1000);
+};
+
+document.querySelector(".reserve-btn").addEventListener("click", handleReservation);
+
+function closeModalFunc() {
+  const modal = document.querySelector('.modal');
+  if (modal) {
+    modal.style.opacity = "0";
+    modal.querySelector(".modal-content").style.opacity = "0";
+    setTimeout(() => {
+      modal.style.display = "none";
+      modal.querySelector("#modal-vid video").setAttribute("src", "");
+    }, 500);
+  }
+}
